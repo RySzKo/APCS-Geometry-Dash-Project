b5d8a5d6305dd4e10cabb6acf481b32fccc70260
mikeliu <mikeliu@10.142.13.102>
2019-05-01 14:43:23 -0700

    NEW

--- GeoRunner.java
+++ GeoRunner.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.util.*;
 import javax.swing.*;
+
 //TEST BY MIKE1
 public class GeoRunner {
 	private JPanel panel;
@@ -8,34 +9,32 @@ public class GeoRunner {
 
 	public static void main(String[] args) {
 		new GeoRunner().start();
-		
-	}
 
+	}
 
 	private void start() {
 		JFrame frame = new JFrame("Geometry Dash");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new JPanel() {
 			@Override
-			
+
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
 				drawGame(g);
 			}
-			
+
 		};
-		
+
 		panel.setBackground(Color.BLUE);
 		panel.setPreferredSize(new Dimension(800, 600));
-		
+
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
-		
-		panel.requestFocusInWindow();
-		
-	}
 
+		panel.requestFocusInWindow();
+
+	}
 
 	protected void drawGame(Graphics g) {
 		g.drawLine(0, 500, 2000, 500);
