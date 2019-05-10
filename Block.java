9e98be4cd8ddca6d71acbd616705613369754d70
Mike Liu <mikeliu@Mikes-MacBook-Pro.local>
2019-05-09 23:31:42 -0700

    Mike change Bloc
    cd -

--- Block.java
+++ Block.java
@@ -3,14 +3,25 @@ import java.util.*;
 import javax.swing.*;
 
 public class Block extends Obstacle {
+
+	Block b;
+	// NEXT FOUR LINES WILL BE DELETED EVENTUALLY
+	int xval = 10;
+	int yval = 450;
+	int bx = 10;
+	int by = 10;
+
+	public void draw(Graphics g) {
+		g.drawRect(xval, yval, bx, by);
+	}
+
 	// UPDATE BY MIKE
 	private int dx, dy, xloc, yloc;
 	private Rectangle hit;
-	
-	//Devang: Updated image
-		public Block(int xSpeed, Image i) {
+
+	// Devang: Updated image
+	public Block(int xSpeed, Image i) {
 		super(xSpeed, "block.png");
 		image = getImage("block.png");
-		}
+	}
 }
-
